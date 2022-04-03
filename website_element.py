a1c213d34bd964dfec142d875e1e9b7543adb682
Charles Ming <charlesming@berkeley.edu>
2022-04-03 13:21:43 -0700

    finish website_element

--- website_element.py
+++ website_element.py
@@ -3,50 +3,50 @@ class WebsiteElement():
     """Glorified xpath, allows us to differentiate between 
     xpaths so our code knows what to do for each given xpath"""
     def __init__(self, element = '', name = '', type = '', need_clear = False):
-        # Element is xpath text.
+        # Element is xpath text. 
         
-        """CODE HERE"""
+        self._element = element
         # Name of xpath and name can be used by send_key if element is a xpath_sendkey
         
-        """CODE HERE"""
+        self._name = name
         # Type of xpath; is it a send key or a regular xpath
         
-        """CODE HERE"""
-        # If im an xpath_sendkey do i need to clear text box before inputting text?                      222
+        self._type = type
+        # If im an xpath_sendkey do i need to clear text box before inputting text?
         
-        """CODE HERE"""
+        self._need_clear = need_clear
     
     @property
     def element(self):
-        """CODE HERE"""
+        return self._element
     @element.setter
     def element(self, element):
-        """CODE HERE"""
+        self._element = element
     @property
     def name(self):
-        """CODE HERE"""
+        return self._name
     @name.setter
     def name(self, name):
-        """CODE HERE"""
+        self._name = name
     @property
     def type(self):
-        """CODE HERE"""
+        return self._type
     @type.setter
     def type(self, type):
-        """CODE HERE"""
+        self._type = type
     @property
     def need_clear(self):
-        """CODE HERE"""
+        return self._need_clear
     @need_clear.setter
     def need_clear(self, boolean):
-        """CODE HERE"""
+        self._need_clear = boolean
     def need_clear_to_str(self):
         """prints need_clear variable to string 
         for hardsaving to json or txt.
         
         variables -- need_clear
         """
-        return 'true' if """CODE HERE""" == True else 'false'
+        return 'true' if self._need_clear == True else 'false'
     def write_self(self):
         """prints information of self for saving to a json or txt"""
         return ' '.join([self.element, self.name, self.type])
