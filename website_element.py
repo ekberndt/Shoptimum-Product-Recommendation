13869af684edc9b9ab1a25bcb6dbfc52a195cb20
Ashley Chang <ashleychang@berkeley.edu>
2022-04-24 19:41:31 -0700

    add website element

--- website_element.py
+++ website_element.py
@@ -1,19 +1,8 @@
-
 class WebsiteElement():
-    """Glorified xpath, allows us to differentiate between 
-    xpaths so our code knows what to do for each given xpath"""
     def __init__(self, element = '', name = '', type = '', need_clear = False):
-        # Element is xpath text. 
-        
         self._element = element
-        # Name of xpath and name can be used by send_key if element is a xpath_sendkey
-        
         self._name = name
-        # Type of xpath; is it a send key or a regular xpath
-        
-        self._type = type
-        # If im an xpath_sendkey do i need to clear text box before inputting text?
-        
+        self._type = type 
         self._need_clear = need_clear
     
     @property
@@ -38,15 +27,9 @@ class WebsiteElement():
     def need_clear(self):
         return self._need_clear
     @need_clear.setter
-    def need_clear(self, boolean):
-        self._need_clear = boolean
+    def need_clear(self, bool):
+        self._need_clear = bool
     def need_clear_to_str(self):
-        """prints need_clear variable to string 
-        for hardsaving to json or txt.
-        
-        variables -- need_clear
-        """
-        return 'true' if self._need_clear == True else 'false'
+        return 'true' if self.need_clear == True else 'false'
     def write_self(self):
-        """prints information of self for saving to a json or txt"""
         return ' '.join([self.element, self.name, self.type])
