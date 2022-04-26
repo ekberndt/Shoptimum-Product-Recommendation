9ab185d6b62b0401a147c695e90a57b863ea875c
Eric Berndt <64391920+ekberndt@users.noreply.github.com>
2022-04-26 13:08:25 -0700

    Update Ebay.py

--- Websites/Ebay.py
+++ Websites/Ebay.py
@@ -6,33 +6,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 
-
 def find_products(search_query):
     url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=" + search_query + "&_sacat=0"
-
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    
     driver.get(url)
-
     driver.implicitly_wait(2)
-
     elements = driver.find_elements(By.XPATH, '//li[contains(@data-view, "mi:")]')
-
     count = 0
-
     ebay_product = []
+    driver.implicitly_wait(0)
 
     for element in elements:
         #count
-        mini = []
-        print(count)
         count += 1
-        
 
         try:
             name = element.find_element(By.XPATH, './/a//h3[@class="s-item__title"]').text
         except:
-            name = ""        
+            name = ""
+                 
         #price
         try: 
             price = element.find_element(By.XPATH, './/span[@class="s-item__price"]').text
@@ -63,9 +55,7 @@ def find_products(search_query):
         except:
             imgLink = ""
 
-
         product = Product.Product(name, price, rating, numberOfRate, count, link, imgLink)
-        
         ebay_product.append(product)
 
     return ebay_product
